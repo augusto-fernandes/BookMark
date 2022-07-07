8d8c436faf057c82c8900841b86f0e98283c7f8b
Augusto Fernandes Costa <augusto.fcosta1808@gmail.com>
2022-07-06 22:58:27 -0300

    iniciando a classe Book

---
+++ src/main/java/com/augusto/bookmark/domain/Book.java
@@ -0,0 +1,40 @@
+package com.augusto.bookmark.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String title;
+    private Integer page;
+    private String link;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(id, book.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
