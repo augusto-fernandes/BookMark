f0723939770e9b308f8d5b1d43b50e94febde08d
Augusto Fernandes Costa <augusto.fcosta1808@gmail.com>
2022-07-06 23:28:15 -0300

    interface book repository

---
+++ src/main/java/com/augusto/bookmark/repository/BookRepository.java
@@ -0,0 +1,11 @@
+package com.augusto.bookmark.repository;
+
+import com.augusto.bookmark.domain.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Integer> {
+}
